e781f663dc84773f7f6d7c31d8957f6279950369
Harish Maneru <harishmaneru44@gmail.com>
2023-09-30 13:39:07 +0530

    ram1

--- src/UserRegistration.js
+++ src/UserRegistration.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 function ColorChangingBox() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -9,51 +10,65 @@ function ColorChangingBox() {
   const [emailError, setEmailError] = useState('');
   const [mobileError, setMobileError] = useState('');
   const navigate = useNavigate();
+
   const handleStartGame = () => {
     // Validation logic
+    let valid = true;
+
     if (name === '') {
       setNameError('Name is required');
-      return;
+      valid = false;
+    } else if (name.length < 3) {
+      setNameError('Name must be at least 3 characters long');
+      valid = false;
     } else {
       setNameError('');
     }
+
     // Email validation
     if (email === '') {
       setEmailError('Email is required');
-      return;
+      valid = false;
     } else if (!isValidEmail(email)) {
       setEmailError('Invalid email address');
-      return;
+      valid = false;
     } else {
       setEmailError('');
     }
+
     // Mobile number validation
     if (mobile === '') {
       setMobileError('Mobile number is required');
-      return;
+      valid = false;
     } else if (!/^\d{10}$/.test(mobile)) {
       setMobileError('Invalid mobile number');
-      return;
+      valid = false;
     } else {
       setMobileError('');
     }
-    let n, y;
-    if (difficulty === 'easy') {
-      n = 10;
-    } else if (difficulty === 'medium') {
-      n = 15;
-    } else {
-      n = 25;
+
+    if (valid) {
+      let n, y;
+      if (difficulty === 'easy') {
+        n = 10;
+      } else if (difficulty === 'medium') {
+        n = 15;
+      } else {
+        n = 25;
+      }
+      y = 40;
+
+      // If all validations pass, navigate to the game page
+      navigate('/GreenLightRedLight', { state: { difficulty, n, y } });
     }
-    y = 40; 
-    // If all validations pass, navigate to the game page
-    navigate('/GreenLightRedLight', { state: { difficulty, n, y } });
   };
+
   const isValidEmail = (email) => {
     // Regular expression for a simple email validation
     const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     return emailPattern.test(email);
   };
+
   return (
     <div className="registration-container">
       <h2>Welcome to the Color Challenge Game!</h2>
